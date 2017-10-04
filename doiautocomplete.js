998719a0d70bcca1a5d840a31a3ab0c7a0b1e70a
Mathieu d'Aquin <mathieu@daquin.net>
2017-10-04 14:48:44 +0100

    Update doiautocomplete.js

--- doiautocomplete.js
+++ doiautocomplete.js
@@ -1,5 +1,5 @@
 
-// add jquery if not there
+// add jquery if not there // this migt not be necessary, i.e. we could remove dependency to jQuery
 if (typeof jQuery == 'undefined') {
   var script = document.createElement('script');
   script.type = "text/javascript";
@@ -14,7 +14,11 @@ createDialog();
 function createDialog(){
   var div = document.createElement('div');
   div.id = "DOIAC_DIALOG";
-  div.style = "position: fixed; top: 200px; left: 20%; width: 60%; background: red; height: 30px; border-radius: 15px;" 
+  div.style = "position: fixed; top: 200px; left: 20%; width: 60%; background: red; height: 30px; border-radius: 15px;" #
+  var st = '<span style="font-weight: bold; color: black; width: 19%;">DOI:</span> '+ 
+        '<input name="DOIAC_DOI_INPUT" id="DOIAC_DOI_INPUT" style="width: 60%" placeholder="copy-paste the DOI of your paper here" />'+ 
+        '<a href="javascript:checkDOI();" style="padding: 10px 10px 10px 10px; background: green; width: 19%; border-radius: 10px">Submit</a>';
+  div.innerHTML=st;
   document.getElementsByTagName('body')[0].appendChild(div);
 }
 
