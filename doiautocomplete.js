9b4a56ed3e4c8e1fb74bee07fb9141a29e674fb1
Mathieu d'Aquin <mathieu@daquin.net>
2017-10-04 14:24:44 +0100

    Update doiautocomplete.js

--- doiautocomplete.js
+++ doiautocomplete.js
@@ -4,7 +4,7 @@ if (typeof $ == 'undefined') {
   var script = document.createElement('script');
   script.type = "text/javascript";
   script.src = "https://code.jquery.com/jquery-3.2.1.min.js";
-  script.integrity = "sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=";
+  // script.integrity = "sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=";
   script.crossorigin="anonymous" 
   document.getElementsByTagName('head')[0].appendChild(script);
 }
